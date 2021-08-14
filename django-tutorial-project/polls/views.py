cfca770b7b04a2be598f0254c568b36c493de633
itolstov09 <itolstov09@gmail.com>
2021-08-14 11:56:17 +0600

    Вместо конструкции try/except использовал django.shortcuts.get_object_or_404. Исправил опечатку поля publication_date. 'Выключил' results, vote для теста работоспособности

--- django-tutorial-project/polls/views.py
+++ django-tutorial-project/polls/views.py
@@ -1,26 +1,29 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from . models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-publication_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, template_name='polls/index.html', context=context)
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question object does not exist')
-    return render(request, template_name='polls/index.html', context={'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(
+        request,
+        template_name='polls/index.html',
+        context={'question': question}
+    )
+
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+    pass
+    # return HttpResponse(f"You're looking at the results of question {question_id}")
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on {question_id}")
+    pass
+    # return HttpResponse(f"You're voting on {question_id}")
 
