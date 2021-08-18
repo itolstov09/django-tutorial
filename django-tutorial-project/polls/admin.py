32167b916ae5a605db677d11deaf5883109f08ff
itolstov09 <itolstov09@gmail.com>
2021-08-18 18:11:23 +0600

    использование django.contrib.admin.StackedInline для модели Choice при отображении внутри страницы модели Question

--- django-tutorial-project/polls/admin.py
+++ django-tutorial-project/polls/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 
-from . models import Question
+from . models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestuionAdmin(admin.ModelAdmin):
     fieldsets = [
             (None, {'fields': ['question_text']}),
-            ('Date information', {'fields': ['pub_date']}),
+            ('Date information', {'fields': ['publication_date']}),
         ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestuionAdmin)
